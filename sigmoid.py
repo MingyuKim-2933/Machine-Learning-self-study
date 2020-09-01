f4b52d5abb28f89891c8558ac0bb5b0e98a699ac
MingyuKim-2933 <68273065+MingyuKim-2933@users.noreply.github.com>
2020-09-01 16:32:43 +0900

    예제 programming by python

---
+++ sigmoid.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def sigmoid(a):
+    return 1 / (1 + np.exp(-x))
+
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
