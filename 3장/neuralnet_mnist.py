9541380a8b7b5c2ca22ab22798287f40a65e01d7
김민규 <kmg2933@naver.com>
2020-09-10 00:24:57 +0900

    배치 처리 추가

--- 3장/neuralnet_mnist.py
+++ 3장/neuralnet_mnist.py
@@ -47,11 +47,12 @@ def predict(network, x):
 x, t = get_data()
 network = init_network()
 
+batch_size = 100 # 배치 크기
 accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다
-    if p == t[i]:
-        accuracy_cnt += 1
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1) # 확률이 가장 높은 원소의 인덱스를 얻는다
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
 
 print("Accuracy:"+str(float(accuracy_cnt) / len(x)))
