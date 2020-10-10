66534bda3f2d848b37c70adc62356c8fd36c6a34
김민규 <kmg2933@naver.com>
2020-10-10 23:54:42 +0900

    4장 upload

---
+++ chap4/numerical_gradient.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def numerical_gradient(f, x): # 기울기
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)  # x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # (f-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
+
+        return grad
