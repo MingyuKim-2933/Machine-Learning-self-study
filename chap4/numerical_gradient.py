f13c6bea8684d176864f36c85aa7369487a13a7a
김민규 <kmg2933@naver.com>
2020-10-11 21:16:05 +0900

    4장 upload

--- chap4/numerical_gradient.py
+++ chap4/numerical_gradient.py
@@ -1,22 +1,52 @@
 import numpy as np
 
 
-def numerical_gradient(f, x):  # 기울기
-    h = 1e-4 # 0.0001
-    grad = np.zeros_like(x)  # x와 형상이 같은 배열을 생성
+def _numerical_gradient_1d(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
 
     for idx in range(x.size):
         tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
 
-        # f(x+h) 계산
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        # (f-h) 계산
         x[idx] = tmp_val - h
-        fxh2 = f(x)
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
 
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val  # 값 복원
+        x[idx] = tmp_val
+
+    return grad
+
+
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_1d(f, x)
 
         return grad
+
+
+def numerical_gradient(f, x):  # 다차원 배열을 처리할 수 있다.
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val
+        it.iternext()
+
+    return grad
